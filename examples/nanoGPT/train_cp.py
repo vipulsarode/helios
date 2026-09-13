3b91cf9fe78c5bc6539ff29a469bb93c56211564
Vipul <vipul.sarode007@gmail.com>
2026-09-13 14:55:58 -0400

    added train_cp.py

--- examples/nanoGPT/train_cp.py
+++ examples/nanoGPT/train_cp.py
@@ -1,13 +1,4 @@
 """
-Modifying training script cloned from https://github.com/karpathy/nanoGPT/ to make it compatible with my implementation of Tensor Parallelism.
-
-"""
-
-"""
-
-"""
-
-"""
 This training script can be run both on a single gpu in debug mode,
 and also in a larger training run with distributed data parallel (ddp).
 
@@ -25,54 +16,39 @@ $ torchrun --nproc_per_node=8 --nnodes=2 --node_rank=1 --master_addr=123.456.123
 (If your cluster does not have Infiniband interconnect prepend NCCL_IB_DISABLE=1)
 """
 
-import sys
-import os
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
-
 import os
 import time
 import math
 import pickle
 from contextlib import nullcontext
-
+import torch.nn as nn
 import numpy as np
 import torch
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.distributed import init_process_group, destroy_process_group
 import torch.distributed as dist
 from model import GPTConfig, GPT
-
-
-from src.parallelism.tensor_parallel.tensor_parallel import shard_model_for_tp
-
-# -----------------------------------------------------------------------------
-# dist.init_process_group(backend='nccl')
-# group = dist.distributed_c10d._get_default_group()
-# -----------------------------------------------------------------------------
-column_parallel_patterns = ["c_fc"]
-row_parallel_patterns = ["c_proj"]
-
-
+from src.parallelism.context_parallel.cp_zigzag_backward import zigzag_attention
 
 # -----------------------------------------------------------------------------
 # default config values designed to train a gpt2 (124M) on OpenWebText
 # I/O
 out_dir = 'out'
-eval_interval = 20 #2000
+eval_interval = 2000
 log_interval = 1
-eval_iters = 10 #200
+eval_iters = 200
 eval_only = False # if True, script exits right after the first eval
 always_save_checkpoint = True # if True, always save a checkpoint after each eval
 init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
 # wandb logging
-wandb_log = True # disabled by default
-wandb_project = 'helios-experiments'
-wandb_run_name = 'gpt2_tp' # 'run' + str(time.time())
+wandb_log = False # disabled by default
+wandb_project = 'owt'
+wandb_run_name = 'gpt2' # 'run' + str(time.time())
 # data
-dataset = 'shakespeare_char'
+dataset = 'openwebtext'
 gradient_accumulation_steps = 5 * 8 # used to simulate larger batch sizes
 batch_size = 12 # if gradient_accumulation_steps > 1, this is the micro-batch size
-block_size = 256 #1024
+block_size = 1024
 # model
 n_layer = 12
 n_head = 12
@@ -92,18 +68,42 @@ warmup_iters = 2000 # how many steps to warm up for
 lr_decay_iters = 600000 # should be ~= max_iters per Chinchilla
 min_lr = 6e-5 # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
 # DDP settings
-backend = 'gloo' # 'nccl', 'gloo', etc.
+backend = 'nccl' # 'nccl', 'gloo', etc.
 # system
-device = 'cpu' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1' etc., or try 'mps' on macbooks
-dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32', 'bfloat16', or 'float16', the latter will auto implement a GradScaler
-compile = False # use PyTorch 2.0 to compile the model to be faster
+device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1' etc., or try 'mps' on macbooks
+dtype = 'float32' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32', 'bfloat16', or 'float16', the latter will auto implement a GradScaler
+compile = True # use PyTorch 2.0 to compile the model to be faster
+
+cp_size = int(os.environ.get('WORLD_SIZE', 1))
+cp_mode = 'zigzag'          # or 'contiguous'
+base_seed = 1337
+
+if cp_size > 1:
+    init_process_group(backend='nccl')
+    cp_rank = int(os.environ['RANK'])
+    local_rank = int(os.environ['LOCAL_RANK'])
+    device = f'cuda:{local_rank}'
+    torch.cuda.set_device(device)
+    master_process = (cp_rank == 0)
+else:
+    cp_rank = 0
+    master_process = True
 # -----------------------------------------------------------------------------
 config_keys = [k for k,v in globals().items() if not k.startswith('_') and isinstance(v, (int, float, bool, str))]
-exec(open('examples/nanoGPT/configurator.py').read()) # overrides from command line or config file
+exec(open('configurator.py').read()) # overrides from command line or config file
 config = {k: globals()[k] for k in config_keys} # will be useful for logging
 # -----------------------------------------------------------------------------
 
-# # various inits, derived attributes, I/O setup
+class CPPositionEmbedding(nn.Module):
+    def __init__(self, wpe, pos_global):
+        super().__init__()
+        self.wpe = wpe                              # the original nn.Embedding
+        self.register_buffer('pos_global', pos_global)
+
+    def forward(self, pos):                         # pos is the wrong arange
+        return self.wpe(self.pos_global)            # ignore it, use the right one
+    
+# various inits, derived attributes, I/O setup
 # ddp = int(os.environ.get('RANK', -1)) != -1 # is this a ddp run?
 # if ddp:
 #     init_process_group(backend=backend)
@@ -124,65 +124,106 @@ config = {k: globals()[k] for k in config_keys} # will be useful for logging
 #     seed_offset = 0
 #     ddp_world_size = 1
 # tokens_per_iter = gradient_accumulation_steps * ddp_world_size * batch_size * block_size
-# print(f"tokens per iteration will be: {tokens_per_iter:,}")
-
-# various inits, derived attributes, I/O setup
-tp = int(os.environ.get('RANK', -1)) != -1 # is this a tp run?
-if tp:
-    init_process_group(backend=backend, init_method="file:///tmp/helios_tp_rendezvous", rank=int(os.environ['RANK']), world_size=int(os.environ['WORLD_SIZE']))
-    tp_rank = int(os.environ['RANK'])
-    tp_local_rank = int(os.environ['LOCAL_RANK'])
-    tp_world_size = int(os.environ['WORLD_SIZE'])
-    group = dist.distributed_c10d._get_default_group()
-    # device = f'cuda:{tp_local_rank}'
-    device = 'cpu'
-    if torch.cuda.is_available():    
-        torch.cuda.set_device(device)
-    master_process = tp_rank == 0 # this process will do logging, checkpointing etc.
-    seed_offset = 0 # we want each rank to get the same data
-    # world_size number of processes will be training simultaneously, so we can scale
-    # down the desired gradient accumulation iterations per process proportionally
-    
-    # gradients are already accumulated in TP ranks. We dont need this.
-    # assert gradient_accumulation_steps % tp_rank == 0
-    # gradient_accumulation_steps //= tp_rank
-else:
-    # if not ddp, we are running on a single gpu, and one process
-    master_process = True
-    seed_offset = 0
-    tp_world_size = 1
-tokens_per_iter = gradient_accumulation_steps * 1 * batch_size * block_size # for tp, we have same data for all, not different 
+tokens_per_iter = gradient_accumulation_steps *  batch_size * block_size
 print(f"tokens per iteration will be: {tokens_per_iter:,}")
 
 if master_process:
     os.makedirs(out_dir, exist_ok=True)
-torch.manual_seed(1337 + seed_offset)
-torch.backends.cuda.matmul.allow_tf32 = True # allow tf32 on matmul
-torch.backends.cudnn.allow_tf32 = True # allow tf32 on cudnn
+torch.manual_seed(1337)   #Makind seed equal for all ranks
+torch.backends.cuda.matmul.allow_tf32 = False # allow tf32 on matmul
+torch.backends.cudnn.allow_tf32 = False # allow tf32 on cudnn
 device_type = 'cuda' if 'cuda' in device else 'cpu' # for later use in torch.autocast
 # note: float16 data type will automatically use a GradScaler
 ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[dtype]
 ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
 
 # poor man's data loader
-# data_dir = os.path.join('data', dataset)
-data_dir = 'examples/nanoGPT/data/shakespeare_char'
-def get_batch(split):
-    # We recreate np.memmap every batch to avoid a memory leak, as per
-    # https://stackoverflow.com/questions/45132940/numpy-memmap-memory-usage-want-to-iterate-once/61472122#61472122
+data_dir = os.path.join('data', dataset)
+# def get_batch(split):
+#     # We recreate np.memmap every batch to avoid a memory leak, as per
+#     # https://stackoverflow.com/questions/45132940/numpy-memmap-memory-usage-want-to-iterate-once/61472122#61472122
+#     if split == 'train':
+#         data = np.memmap(os.path.join(data_dir, 'train.bin'), dtype=np.uint16, mode='r')
+#     else:
+#         data = np.memmap(os.path.join(data_dir, 'val.bin'), dtype=np.uint16, mode='r')
+#     ix = torch.randint(len(data) - block_size, (batch_size,))
+#     x = torch.stack([torch.from_numpy((data[i:i+block_size]).astype(np.int64)) for i in ix])
+#     y = torch.stack([torch.from_numpy((data[i+1:i+1+block_size]).astype(np.int64)) for i in ix])
+#     if device_type == 'cuda':
+#         # pin arrays x,y, which allows us to move them to GPU asynchronously (non_blocking=True)
+#         x, y = x.pin_memory().to(device, non_blocking=True), y.pin_memory().to(device, non_blocking=True)
+#     else:
+#         x, y = x.to(device), y.to(device)
+#     return x, y
+
+base_seed = 42
+_batch_gen = torch.Generator()  # CPU generator, module level
+
+def zigzag_split(t, cp_rank, cp_size):
+    chunks = t.chunk(2 * cp_size, dim=1)
+    return torch.cat([chunks[cp_rank], chunks[2 * cp_size - 1 - cp_rank]], dim=1)
+
+def contiguous_split(t, cp_rank, cp_size):
+    return t.chunk(cp_size, dim=1)[cp_rank]
+
+def get_batch(split, iter_num):
     if split == 'train':
         data = np.memmap(os.path.join(data_dir, 'train.bin'), dtype=np.uint16, mode='r')
     else:
         data = np.memmap(os.path.join(data_dir, 'val.bin'), dtype=np.uint16, mode='r')
-    ix = torch.randint(len(data) - block_size, (batch_size,))
+
+
+    seed = (base_seed
+            + iter_num * 0x9E3779B1
+            + (0 if split == 'train' else 0x5BF03635)) & 0x7FFFFFFFFFFFFFFF
+    _batch_gen.manual_seed(seed)
+
+    ix = torch.randint(len(data) - block_size, (batch_size,), generator=_batch_gen)
+
+    # unchanged from here
     x = torch.stack([torch.from_numpy((data[i:i+block_size]).astype(np.int64)) for i in ix])
     y = torch.stack([torch.from_numpy((data[i+1:i+1+block_size]).astype(np.int64)) for i in ix])
     if device_type == 'cuda':
-        # pin arrays x,y, which allows us to move them to GPU asynchronously (non_blocking=True)
         x, y = x.pin_memory().to(device, non_blocking=True), y.pin_memory().to(device, non_blocking=True)
     else:
         x, y = x.to(device), y.to(device)
+
+    if cp_size > 1:
+        split_fn = zigzag_split if cp_mode == 'zigzag' else contiguous_split
+        x = split_fn(x, cp_rank, cp_size)
+        y = split_fn(y, cp_rank, cp_size)
+
     return x, y
+
+class CPAttention(nn.Module):
+    def __init__(self, attn, cp_group, cp_size, cp_rank):
+        super().__init__()
+        self.c_attn = attn.c_attn          # reuse the trained weights
+        self.c_proj = attn.c_proj
+        self.n_head = attn.n_head
+        self.n_embd = attn.n_embd
+        self.cp_group, self.cp_size, self.cp_rank = cp_group, cp_size, cp_rank
+        self.send_to = (cp_rank + 1) % cp_size
+        self.recv_from = (cp_rank - 1) % cp_size
+
+    def forward(self, x):
+        B, T, C = x.size()
+        q, k, v = self.c_attn(x).split(self.n_embd, dim=2)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+
+        q_h, q_t = q.chunk(2, dim=2)
+        k_h, k_t = k.chunk(2, dim=2)
+        v_h, v_t = v.chunk(2, dim=2)
+
+        o_h, o_t = zigzag_attention(q_h, q_t, k_h, k_t, v_h, v_t,
+                                    self.cp_group, self.cp_size,
+                                    self.send_to, self.recv_from, self.cp_rank)
+
+        y = torch.cat([o_h, o_t], dim=2)
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
+        return self.c_proj(y)
 
 # init these up here, can override if init_from='resume' (i.e. from a checkpoint)
 iter_num = 0
@@ -246,16 +287,15 @@ if block_size < model.config.block_size:
     model_args['block_size'] = block_size # so that the checkpoint will have the right value
 model.to(device)
 
-#### Modify model and layers for Tensor Parallelism #####################################################
-tp_model = shard_model_for_tp(model, group = group, column_parallel_patterns=column_parallel_patterns, row_parallel_patterns=row_parallel_patterns)
-
-for name, layer in tp_model.named_modules():
-    if isinstance(layer, torch.nn.Linear):
-        print(f"Layer {name} is a Linear layer")
-
+for block in model.transformer.h:
+    block.attn = CPAttention(block.attn, cp_group, cp_size, cp_rank)
 
 # initialize a GradScaler. If enabled=False scaler is a no-op
 scaler = torch.amp.GradScaler(enabled=(dtype == 'float16'))
+
+if cp_size > 1:
+    pos_global = zigzag_split(torch.arange(block_size).unsqueeze(0), cp_rank, cp_size).squeeze(0).to(device)
+    model.transformer.wpe = CPPositionEmbedding(model.transformer.wpe, pos_global)
 
 # optimizer
 optimizer = model.configure_optimizers(weight_decay, learning_rate, (beta1, beta2), device_type)
@@ -269,12 +309,17 @@ if compile:
     unoptimized_model = model
     model = torch.compile(model) # requires PyTorch 2.0
 
-# we don't need DDP container fr TP
 # wrap model into DDP container
-# if ddp:
-    # model = DDP(model, device_ids=[ddp_local_rank])
+if ddp:
+    model = DDP(model, device_ids=[ddp_local_rank])
 
 # helps estimate an arbitrarily accurate loss over either split using many batches
+val_data = np.memmap(os.path.join(data_dir, 'val.bin'), dtype=np.uint16, mode='r')
+_val_gen = torch.Generator().manual_seed(12345)
+val_offsets = torch.randint(len(val_data) - block_size, (eval_iters * batch_size,),
+                            generator=_val_gen)
+
+
 @torch.no_grad()
 def estimate_loss():
     out = {}
@@ -282,9 +327,20 @@ def estimate_loss():
     for split in ['train', 'val']:
         losses = torch.zeros(eval_iters)
         for k in range(eval_iters):
-            X, Y = get_batch(split)
+            if split == 'val':
+                ix = val_offsets[k * batch_size:(k + 1) * batch_size]
+                X = torch.stack([torch.from_numpy((val_data[i:i+block_size]).astype(np.int64)) for i in ix])
+                Y = torch.stack([torch.from_numpy((val_data[i+1:i+1+block_size]).astype(np.int64)) for i in ix])
+                X, Y = X.to(device), Y.to(device)
+                if cp_size > 1:
+                    split_fn = zigzag_split if cp_mode == 'zigzag' else contiguous_split
+                    X, Y = split_fn(X, cp_rank, cp_size), split_fn(Y, cp_rank, cp_size)
+            else:
+                X, Y = get_batch(split, k)
             with ctx:
                 logits, loss = model(X, Y)
+            if cp_size > 1:
+                dist.all_reduce(loss, op=dist.ReduceOp.AVG, group=cp_group)
             losses[k] = loss.item()
         out[split] = losses.mean()
     model.train()
@@ -313,9 +369,7 @@ if wandb_log and master_process:
 X, Y = get_batch('train') # fetch the very first batch
 t0 = time.time()
 local_iter_num = 0 # number of iterations in the lifetime of this process
-
-# raw_model = model.module if ddp else model # unwrap DDP container if needed
-raw_model = tp_model # we already have our model sharded for tp
+raw_model = model.module if ddp else model # unwrap DDP container if needed
 running_mfu = -1.0
 while True:
 
@@ -355,7 +409,7 @@ while True:
     # forward backward update, with optional gradient accumulation to simulate larger batch size
     # and using the GradScaler if data type is float16
     for micro_step in range(gradient_accumulation_steps):
-        # if ddp: # running tp specific, don't need ddp
+        # if ddp:
         #     # in DDP training we only need to sync gradients at the last micro step.
         #     # the official way to do this is with model.no_sync() context manager, but
         #     # I really dislike that this bloats the code and forces us to repeat code
@@ -385,7 +439,13 @@ while True:
     if iter_num % log_interval == 0 and master_process:
         # get loss as float. note: this is a CPU-GPU sync point
         # scale up to undo the division above, approximating the true total loss (exact would have been a sum)
-        lossf = loss.item() * gradient_accumulation_steps
+        if cp_size > 1:
+            loss_report = loss.detach() * gradient_accumulation_steps
+            dist.all_reduce(loss_report, op=dist.ReduceOp.AVG, group=cp_group)
+            lossf = loss_report.item()
+        else:
+            lossf = loss.item() * gradient_accumulation_steps
+        # lossf = loss.item() * gradient_accumulation_steps
         if local_iter_num >= 5: # let the training loop settle a bit
             mfu = raw_model.estimate_mfu(batch_size * gradient_accumulation_steps, dt)
             running_mfu = mfu if running_mfu == -1.0 else 0.9*running_mfu + 0.1*mfu
@@ -397,5 +457,5 @@ while True:
     if iter_num > max_iters:
         break
 
-if tp:
+if ddp:
     destroy_process_group()
